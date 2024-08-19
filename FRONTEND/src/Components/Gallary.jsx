71826d8dc302b1ba6d8dac7400fd605a65f70759
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-08-19 18:59:02 +0530

    dragging feature improved using dsa

--- FRONTEND/src/Components/Gallary.jsx
+++ FRONTEND/src/Components/Gallary.jsx
@@ -5,7 +5,9 @@ import Navbar from './Navbar'
 import { LinkedList } from '../utils/DragDrop';
 const img = "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 const img1 = 'https://i.pinimg.com/564x/4b/77/12/4b771221629ca9d264393973faa29ff4.jpg'
-const images = [img1, img, img1, img, img, img, img, img, img, img, img, img1]
+const img2 = 'https://i.pinimg.com/564x/f4/01/33/f40133e28a8928d38ca5768596daad25.jpg'
+const img4 = 'https://i.pinimg.com/564x/cc/15/23/cc152387fe66349b720991ae97592cea.jpg'
+const images = [img1, img, img1, img, img2, img, img, img4, img, img4, img2, img1]
 function Gallary() {
   useEffect(() => {
     const container = document.querySelector(".container");
@@ -13,9 +15,10 @@ function Gallary() {
     // console.log(container)
     const list = new LinkedList()
     boxes.forEach(box => list.append(box));
-    let draggedElementNode = null;
+    let draggedElementNode = null,dragged=null;
     boxes.forEach(box => {
       box.addEventListener('dragstart', (event) => {
+        dragged=event.target;
         draggedElementNode = list.find(event.target);
         event.dataTransfer.effectAllowed = "move";
         event.target.style.opacity = '0.5';
@@ -28,10 +31,14 @@ function Gallary() {
       // Event listener for drop
       box.addEventListener('drop', (event) => {
         event.preventDefault();
+        const target=event.currentTarget;
         const targetNode = list.find(event.currentTarget);
 
         if (draggedElementNode && targetNode) {
-          list.swap(draggedElementNode, targetNode, container);
+          if (list.isBefore(dragged, target))
+            list.swap_reverse(draggedElementNode, targetNode, container);
+          else
+            list.swap(draggedElementNode, targetNode, container)
         }
 
         draggedElementNode = null;
