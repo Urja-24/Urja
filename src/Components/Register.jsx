c01311621e411333dc9469b54e451c735f0b83b1
Somesh Dwivedi <sd10072004@gmail.com>
2024-09-02 17:09:57 +0530

    Minor fixes

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -6,8 +6,21 @@ import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
   const { id } = useParams();
-  const backendUrl = 'https://urjabackend.onrender.com';
-  const sports = ["Cricket", "Football", "Hockey"];
+   const backendUrl = "https://urjabackend.onrender.com";
+  //const backendUrl = "http://localhost:3080";
+  const sports = [
+    "Basketball",
+    "Football",
+    "Cricket",
+    "Tennis",
+    "Yoga",
+    "Chess",
+    "Athletics",
+    "Hockey",
+    "Badminton",
+    "Volleyball",
+    "Table Tennis",
+  ];
   const branches = ["CE", "CSE", "MME", "ECE", "ME", "PIE+ECM", "PG", "EE"];
   const [formData, setFormData] = useState({
     playerName: "",
@@ -42,7 +55,7 @@ function Register() {
         console.error("Failed to submit form:", response.statusText);
       }
     } catch (error) {
-      toast.error("Error submitting form!");
+      toast.error("Cannot register twice !");
       console.error("Error submitting form:", error);
     } finally {
       setFormData({
