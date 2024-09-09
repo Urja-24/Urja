0074c32452642de43e7066643006b85ee53b28c1
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-09-09 23:53:54 +0530

    Removed recent forms section

--- src/Components/CustomSportsTable.jsx
+++ src/Components/CustomSportsTable.jsx
@@ -13,7 +13,6 @@ const tableData = [
         Lose: "",
         Draw: "1",
         Points: "",
-        RecentForm:["W","D","L"],
     },
     {
             Team: "ECE",
@@ -22,7 +21,6 @@ const tableData = [
             Lose: "",
             Draw: "1",
             Points: "5",
-            RecentForm:["W","D","L"],
     },
     {
             Team: "EE",
@@ -31,7 +29,6 @@ const tableData = [
             Lose: "",
             Draw: "",
             Points: "",
-            RecentForm:["W","W","L"],
     },
     {
             Team: "ME",
@@ -40,7 +37,6 @@ const tableData = [
             Lose: "",
             Draw: "3",
             Points: "5",
-            RecentForm:["D","D","L"],
     },
     {
             Team: "CE",
@@ -49,7 +45,6 @@ const tableData = [
             Lose: "",
             Draw: "3",
             Points: "5",
-            RecentForm:["D","D","L"],
     },
     {
             Team: "MME",
@@ -58,7 +53,6 @@ const tableData = [
             Lose: "",
             Draw: "3",
             Points: "5",
-            RecentForm:["D","L","L"],
     },
     {
             Team: "PG",
@@ -67,7 +61,6 @@ const tableData = [
             Lose: "",
             Draw: "3",
             Points: "5",
-            RecentForm:["W","D","L"],
     },
 ];
 
@@ -104,7 +97,6 @@ const tableData = [
               <th className="border border-gray-200 px-4 py-2">Lose</th>
                 <th className="border border-gray-200 px-4 py-2">Draw</th>
                 <th className="border border-gray-200 px-4 py-2">Points</th>
-                <th className="border border-gray-200 px-4 py-2">RECENT FORM</th>
             </tr>
           </thead>
           <tbody className="text-white">
@@ -127,13 +119,6 @@ const tableData = [
                 </td>
                 <td className="border border-gray-200 px-4 py-2 text-center">
                   {row.Points}
-                </td>
-                <td className="border flex justify-center items-center gap-3 border-gray-200 px-4 py-2 text-center">
-                  {row.RecentForm.map((form, i) => (
-                    <span key={i} className="text-xl mx-1">
-                        {form === "W" ? <FcApproval size={24} /> : form === "L" ? <FaCircleXmark color="red" />:<AiFillStop />}
-                    </span>
-                  ))}
                 </td>
               </tr>
             ))}
